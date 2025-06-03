cd055c1d8b42c5dc12be23d430f6faf231bfce5b
Abedalqader Alkhatib <aboodjob@gmail.com>
2025-06-03 09:43:35 +0300

    add authintication + build web + auth rebuild

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "738630db641f337bc3e821f0199199fb
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "124616b0abc724a8834c63ecb87c1c80",
+"flutter_bootstrap.js": "7ecbe080edc33a2052b8d962d04ce8a6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
